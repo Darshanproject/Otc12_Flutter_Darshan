aa1dd2deac5049d5227fb5b2fac5b76faf1a707f
Darshanproject <gadadarshan6184@gmail.com>
2023-10-27 15:30:22 +0530

    27-10-2023

--- Ui Deign/myapp1/lib/main.dart
+++ Ui Deign/myapp1/lib/main.dart
@@ -1,18 +1,103 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:myapp1/Screens/task1.dart';
+// void main(){
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Task()
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-import 'package:myapp1/Screens/task1.dart';
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Sentence Reversal'),
+        ),
+        body: SentenceReversalWidget(),
+      ),
+    );
+  }
+}
+
+class SentenceReversalWidget extends StatefulWidget {
+  @override
+  _SentenceReversalWidgetState createState() => _SentenceReversalWidgetState();
+}
+
+class _SentenceReversalWidgetState extends State<SentenceReversalWidget> {
+  String inputText = "";
+  int skipNumber = 0;
+  String outputText = "";
+
+  void reverseSentences() {
+    List<String> sentences = inputText.split('.');
+
+    for (int i = 0; i < sentences.length - 1; i++) {
+      List<String> words = sentences[i].trim().split(' ');
+      int skip = skipNumber;
+      if (skip > words.length) {
+        skip = words.length;
+      }
+      List<String> reversedSentence =
+          words.sublist(0, words.length - skip).reversed.toList();
+      reversedSentence.addAll(words.sublist(words.length - skip));
+      sentences[i] = reversedSentence.join(' ');
+    }
+
+    outputText = sentences.join('. ');
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Task()
-    );
-  }
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            maxLines: 5,
+            onChanged: (value) {
+              inputText = value;
+            },
+            decoration: InputDecoration(labelText: 'Input Paragraph'),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              skipNumber = int.tryParse(value) ?? 0;
+            },
+            decoration: InputDecoration(labelText: 'Skip Number'),
+          ),
+          ElevatedButton(
+            onPressed: reverseSentences,
+            child: Text('Reverse Sentences'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Output Paragraph:',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(outputText),
+        ],
+     ),
+);
+}
 }
